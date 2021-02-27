8ad0e900a5a7a8cc3a5694fdbf941525d4e266d7
Javier Eche <soportejjsystems@gmail.com>
2021-02-27 01:49:42 -0500

    Added Checkout page and testimonial component into Details page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Checkout from './components/pages/Checkout';
 import Details from './components/pages/Details';
 import Home from './components/pages/Home';
 
 function App(): JSX.Element {
   return (
     <div className="container mx-auto px-9 pcb">
-      <Details />
+      <Checkout />
     </div>
   );
 }
---
+++ src/components/pages/Checkout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeaderDark from '../UI/HeaderDark';
+
+const Checkout = () => {
+  return (
+    <div>
+      <HeaderDark />
+    </div>
+  );
+}
+
+export default Checkout;
