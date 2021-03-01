f22108663dbe62154ec2bbb1b77542fdf4166339
Javier Eche <javier.eche1205@gmail.com>
2021-02-28 20:32:15 -0500

    Fixed some errors into Headers

--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import urlIconGlobe from './../../assets/images/globe-b2.svg';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div className="flex justify-between py-4">
-      <h3 className="text-xl font-black">Crashcourse</h3>
+      <Link to="/">
+        <div className="flex gap-2 items-center">
+          <img src={urlIconGlobe} alt=""/>
+          <h3 className="text-xl font-black">Crashcourse</h3>
+        </div>
+      </Link>
       <button className="border-purple font-semibold py-2 px-4 hover:bg-gray-900 hover:text-white text-dark rounded">Registrate Gratis</button>
     </div>
   );
--- src/components/UI/HeaderDark.tsx
+++ src/components/UI/HeaderDark.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import urlIconWorld from './../../assets/images/globe-2.svg';
 
 const HeaderDark = () => {
   return (
-    <div className="h-72px flex justify-center items-center bg-dark gap-2">
-      <img src={urlIconWorld} alt=""/>
-      <h3 className="text-xl text-white font-black">Crashcourse</h3>
-    </div>
+    <Link to="/">
+      <div className="h-72px flex justify-center items-center bg-dark gap-2">
+        <img src={urlIconWorld} alt=""/>
+        <h3 className="text-xl text-white font-black">Crashcourse</h3>
+      </div>
+    </Link>
   );
 }
 
